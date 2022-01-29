dcd8a0e328f07ac688503ccd91b683aa8900c90b
Marcio Sousa <sousa-marcio@live.com>
2022-01-29 01:51:08 -0300

    Criando a classe do player

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-console.log(canvas);
+canvas.width = innerWidth
+canvas.height = innerHeight
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+player.draw()
