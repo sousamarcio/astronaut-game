9da193b2b2e0c7099e01123d21d5102df650b61c
Marcio Sousa <sousa-marcio@live.com>
2022-02-01 01:28:00 -0300

    Criando colisão do player com a plataforma

--- index.js
+++ index.js
@@ -37,7 +37,26 @@ class Player {
     }
 }
 
+class Platform {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 100
+        }
+
+        this.width = 200
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 const player = new Player()
+const platform = new Platform()
+
 const keys = {
     right: {
         pressed: false
@@ -51,13 +70,23 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
-    // Define a velocidade para trás e para frente
+    // Define a velocidade do player para trás e para frente
     if (keys.right.pressed) {
         player.velocity.x = 5
     } else if (keys.left.pressed) {
         player.velocity.x = -5
     } else player.velocity.x = 0
+
+    // Verifica a posição do player para detectar colisão com a plataforma
+    if (player.position.y + player.height <= platform.position.y
+        && player.position.y + player.height + player.velocity.y >=
+        platform.position.y && player.position.x + player.width >=
+        platform.position.x && player.position.x <= platform.position.x +
+        platform.width) {
+        player.velocity.y = 0
+    }
 }
 
 animate()
@@ -108,6 +137,4 @@ window.addEventListener('keyup', ({ keyCode }) => {
             player.velocity.y -= 20
             break
     }
-
-    console.log(keys.right.pressed)
 })
