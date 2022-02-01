9c734860fbc6cdda617cac2c89c95d11458b0d24
Marcio Sousa <sousa-marcio@live.com>
2022-02-01 00:05:58 -0300

    Criando a gravidade no game

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+const gravity = 1.5
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
         }
         this.width = 30
         this.height = 30
@@ -17,7 +23,23 @@ class Player {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
 }
 
 const player = new Player()
-player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}
+
+animate()
